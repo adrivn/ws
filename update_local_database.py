20ad859bbc49af025acd98ff118f5b654e1478ea
adrivn <adrian.emege@gmail.com>
2023-07-21 18:48:59 +0200

    Print out update database status

--- update_local_database.py
+++ update_local_database.py
@@ -7,6 +7,7 @@ files = Path(DIR_PARQUET).glob("*.parquet")
 def main():
     with duckdb.connect(DATABASE_FILE.as_posix()) as bd:
         for parquet_file in files:
+            print(f"Updating database: {parquet_file.stem} ")
             bd.execute(f"CREATE OR REPLACE TABLE {parquet_file.stem} AS SELECT * FROM parquet_scan('{parquet_file}')")
 
 if __name__ == "__main__":
