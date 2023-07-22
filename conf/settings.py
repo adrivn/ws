b71cddcbf6436bf14002687be0b449a7dc476d02
adrivn <adrian.emege@gmail.com>
2023-07-22 20:10:12 +0200

    Added strats and styles config

--- conf/settings.py
+++ conf/settings.py
@@ -1,10 +1,11 @@
-from pathlib import Path
+from pathlib import Path, WindowsPath
 from dataclasses import dataclass
 import datetime
 
 # Configuracion de carpetas y directorios
 BASE_DIR = Path().cwd()
 CONF_DIR = BASE_DIR / "conf"
+QUERIES_DIR = BASE_DIR / "queries"
 DIR_PARQUET = Path("N:/CoralHudson/6. Stock/Parquet").absolute()
 DIR_OUTPUT_LOCAL = Path("_output/").absolute()
 DIR_INPUT_LOCAL = Path("_attachments/").absolute()
@@ -24,7 +25,10 @@ class FileSettings:
     output_dir: str
     output_file: str
     sheet_name: str
+    strat_sheet: str
     header_start: int
+    areas_to_style: dict = None
+    styles_file: WindowsPath = Path(CONF_DIR) / "styles.json"
     db_file: str = DATABASE_FILE.as_posix()
 
     def get_filename(self):
@@ -38,7 +42,8 @@ stockconf = FileSettings(
     DIR_OFFERS.parent / ".outputs",
     "_WS_Stock.xlsx",
     "Wholesale Stock",
-    6
+    "Strats",
+    6,
 )
 
 offersconf = FileSettings(
@@ -46,7 +51,8 @@ offersconf = FileSettings(
     DIR_OFFERS.parent / ".outputs",
     "_Coral_Homes_Offers_Data.xlsx",
     "Offers Data",
-    6
+    "Strats",
+    6,
 )
 
 pipeconf = FileSettings(
@@ -54,6 +60,7 @@ pipeconf = FileSettings(
     DIR_OFFERS.parent / ".outputs",
     "_WS_Pipeline.xlsx",
     "Pipeline 2023",
-    6
+    "Strats",
+    6,
 )
 
