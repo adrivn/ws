294d6ef5ebfeee2cf4cd23637d454e02ba6c61ec
adrivn <adrian.emege@gmail.com>
2023-07-25 09:48:23 +0200

    Updated requirements and Start.ps1 to check for venvs and install if not

--- start.ps1
+++ start.ps1
@@ -10,9 +10,20 @@ $scriptPath = "./start.py"
 Write-Host "Fetching updates..."
 git pull
 
-# Activate the virtual environment
-Write-Host "Activating Python virtual environment..."
-& $venvPath\Scripts\Activate.ps1
+# Check if virtual environment exists, and creates one if not
+if (Test-Path $venvPath\Scripts\Activate.ps1 -PathType Leaf) {
+  Write-Host "Found virtual environment in $venvPath"
+  # Activate the virtual environment
+  Write-Host "Activating Python virtual environment..."
+  & $venvPath\Scripts\Activate.ps1
+}
+else {
+  Write-Host "No virtual environment found. Creating a new one in: $venvPath"
+  python -m venv $venvPath
+  Write-Host "Activating Python virtual environment..."
+  & $venvPath\Scripts\Activate.ps1
+  python install -r conf/requirements.txt
+}
 
 # Check if the virtual environment is activated
 if ($env:VIRTUAL_ENV -eq $null) {
