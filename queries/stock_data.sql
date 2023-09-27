cf9c30ae4bfd227ed18b17ea96e880647c6f9572
adrivn <adrian.emege@gmail.com>
2023-09-27 17:03:51 +0200

    Actual WS perimeter achieved?

--- queries/stock_data.sql
+++ queries/stock_data.sql
@@ -8,17 +8,19 @@ select
         else 'Unidad Registral'
     end as tipo_agrupacion,
     lat.category as categories,
+    lat.source_label,
     trim(mode(w.estrategia)) as strategy,
     (count(*) filter (where m.updatedcategory = 'Sold Assets') * 1.0 / count(*)) as percent_sold,
     (count(*) filter (where m.updatedcategory = 'Sale Agreed') * 1.0 / count(*)) as percent_committed,
     (count(*) filter (where m.updatedcategory = 'Remaining Stock') * 1.0 / count(*)) as percent_stock,
-    trim(mode(m.ur_current::int)) as ur_current,
-    trim(mode(m.commercialdev::int)) as commercialdev,
-    trim(mode(m.jointdev::int)) as jointdev,
+  -- WARN: Seguro que tenemos que usar moda? En lugar de un listado de URs/promos unicas?
+    array_to_string(list_distinct(array_agg(trim(m.ur_current::int))), ' | ') as ur_current,
+    array_to_string(list_distinct(array_agg(trim(m.commercialdev::int))), ' | ') as commercialdev,
+    array_to_string(list_distinct(array_agg(trim(m.jointdev::int))), ' | ') as jointdev,
     trim(mode(m.address)) as address,
     trim(mode(m.town)) as municipality,
     trim(mode(m.city)) as province,
-    trim(mode(m.cadastralreference)) as cadastralreference,
+    array_to_string(list_distinct(array_agg(trim(m.cadastralreference))), ' | ') as cadastralreference,
     trim(mode(m.direccion_territorial)) as dts,
     sum(coalesce(m.currentsaleprice, m.last_listing_price)) as listed_price,
     max(m.last_listing_date) as latest_listing_date,
@@ -44,30 +46,46 @@ select
     sum(m.nbv) filter (where m.updatedcategory = 'Remaining Stock') as remaining_nbv
 
 from master_tape as m
+left join (select UNIDAD_REGISTRAL, CHANNEL, SNAPSHOT_DATE FROM channels_historic as outie 
+            where SNAPSHOT_DATE = (SELECT MAX(SNAPSHOT_DATE)
+                                    FROM channels_historic as innie WHERE
+                                    innie.UNIDAD_REGISTRAL=outie.UNIDAD_REGISTRAL)) ch 
+on m.ur_current = ch.UNIDAD_REGISTRAL
 left join disaggregated_assets as w
     on m.ur_current = w.unidad_registral,
     lateral(
         select
             coalesce(w.unidad_registral, nullif(commercialdev, 0), nullif(jointdev, 0), ur_current)::int as asset_id,
-            case
-                when label is not null
-                    then
-                        case
-                            when publishingchannel_description is null
-                                then 'Wholesale'
-                            when publishingchannel_description = 'Retail'
-                                then 'No longer in Wholesale'
-                            else publishingchannel_description
-                        end
-                else 
-                    case when publishingchannel_description = 'Wholesale'
-                      then 'Wholesale - from Retail'
-                    end
-            end as category
+            -- NOTE: Usar el "CHANNEL" para determinar la pertenencia a un canal u otro (operaciones)
+            -- Asimismo, el canal de venta SEGURO para mayorista es.. Mayorista. Todo lo demás va a Retail.
+            case when ch.CHANNEL is null then
+              case when m.salechannel = 'Mayorista' then 'Wholesale' else 'Retail' end
+            else ch.CHANNEL
+            end as category,
+            case when ch.CHANNEL is null then
+              case when m.salechannel is not null then 'Sale Channel (Reporting)' end
+            else 'Operations Tape'
+            end as source_label,
+            -- case
+                -- when label is not null
+                --     then
+                --         case
+                --             when publishingchannel_description is null
+                --                 then 'Wholesale'
+                --             when publishingchannel_description = 'Retail'
+                --                 then 'No longer in Wholesale'
+                --             else publishingchannel_description
+                --         end
+                -- else 
+                --     case when publishingchannel_description = 'Wholesale'
+                --       then 'Wholesale - from Retail'
+                --     end
+            -- end as category
     ) as lat
 where
      m.updatedcategory != 'Exclusions'
 and
-    category in ( 'Wholesale', 'Wholesale - from Retail', 'No longer in Wholesale' )
+    -- category in ( 'Wholesale', 'Wholesale - from Retail', 'No longer in Wholesale' )
+    category == 'Wholesale'
 group by all
 order by 1, 2
